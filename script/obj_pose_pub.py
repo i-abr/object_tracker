0e237ac4f8d6b99893fb61e3d9cfb71f31dc61be
Ian <iabr4073@gmail.com>
2019-08-22 16:33:35 -0500

    push any updates

--- script/obj_pose_pub.py
+++ script/obj_pose_pub.py
@@ -8,15 +8,22 @@ from geometry_msgs.msg import Pose
 
 if __name__ == '__main__':
     rospy.init_node('obj_pose_pub')
-
+    
+    br = tf.TransformBroadcaster()
     listener = tf.TransformListener()
 
     obj_pose_pub = rospy.Publisher('/object_pose', Pose, queue_size=1)
 
     rate = rospy.Rate(30.0)
     while not rospy.is_shutdown():
+        br.sendTransform(
+                        (0., 0., -0.055),
+                        (0., 0., 0., 1.),
+                        rospy.Time.now(),
+                        "/object_center","/object") 
         try:
-            (trans, rot) = listener.lookupTransform('/origin', '/object', rospy.Time(0))
+            (trans, rot) = listener.lookupTransform('/origin', '/object_center', rospy.Time(0))
+            #(trans, rot) = listener.lookupTransform('/object', '/origin', rospy.Time(0))
             pose_msg = Pose()
 
             pose_msg.position.x = trans[0]
@@ -26,7 +33,7 @@ if __name__ == '__main__':
             pose_msg.orientation.x = rot[0]
             pose_msg.orientation.y = rot[1]
             pose_msg.orientation.z = rot[2]
-            pose_msg.orientation.w = rot[2]
+            pose_msg.orientation.w = rot[3]
             obj_pose_pub.publish(pose_msg)
 
 
